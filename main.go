df78e95bfdd96dbfc8ff40858e91504a701cd0b0
Shafik Amin <shafik23@gmail.com>
2021-07-11 01:40:13 -0700

    Got first draft working.

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
 	"time"
 )
 
@@ -14,19 +14,29 @@ var apiKey = "f5c9e47015ecd81423b5a65aef0240e9da17b921"
 var serviceIds = []string{"7749", "1658", "7750"}
 
 func main() {
-	fmt.Println(siteid, apiKey, serviceIds)
+	// fmt.Println(siteid, apiKey, serviceIds)
 
-	fmt.Println(wsCall("Services/GetServices", map[string]string{
-		"response_type": "json",
-		"service_id":    strings.Join(serviceIds, ",")}))
+	for _, serviceId := range serviceIds {
+		jsonResponseMap := wsCall("Appointments/GetOpenDates", map[string]string{
+			"response_type": "json",
+			"service_id":    serviceId,
+			"num_days":      "20"})
 
+		dates := jsonResponseMap["data"].([]interface{})
+		firstResult := dates[0].(map[string]interface{})
+
+		firstAvailableDate := firstResult["date"].(string)
+
+		fmt.Println("Service ID, First Available Date:", serviceId, firstAvailableDate)
+		fmt.Println("--------")
+	}
 }
 
-func wsCall(endpoint string, params map[string]string) string {
+func wsCall(endpoint string, params map[string]string) map[string]interface{} {
 	base := "https://ws.appointment-plus.com/"
 	baseEndpoint := base + endpoint
 
-	apiGun := &http.Client{Timeout: time.Second * 10}
+	apiGun := &http.Client{Timeout: time.Second * 30}
 	paramValues := url.Values{}
 
 	for k, v := range params {
@@ -38,22 +48,25 @@ func wsCall(endpoint string, params map[string]string) string {
 	req, err := http.NewRequest("POST", fullURL, nil)
 
 	if err != nil {
-		return fmt.Errorf("Got error %s", err.Error()).Error()
+		panic(fmt.Errorf("Could not build URL: %s", err.Error()).Error())
 	}
 
 	req.SetBasicAuth(siteid, apiKey)
 
 	res, err := apiGun.Do(req)
 	if err != nil {
-		return fmt.Errorf("Got error %s", err.Error()).Error()
+		panic(fmt.Errorf("API error from WebServices: %s", err.Error()).Error())
 	}
 
 	defer res.Body.Close()
 
 	result, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return err.Error()
+		panic(fmt.Errorf("Error reading response body: %s", err.Error()).Error())
 	}
 
-	return string(result)
+	jsonMap := make(map[string]interface{})
+	json.Unmarshal([]byte(result), &jsonMap)
+
+	return jsonMap
 }
